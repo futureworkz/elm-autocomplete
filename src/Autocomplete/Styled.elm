f177f0a495a39ce0b1e82deac8d3beef0620f0af
Iker <tdtrinh.web@gmail.com>
2022-08-27 15:02:04 +0700

    Add styled view

--- src/Autocomplete/Styled.elm
+++ src/Autocomplete/Styled.elm
@@ -1,19 +1,100 @@
-module Autocomplete.Styled exposing (input)
+module Autocomplete.Styled exposing
+    ( Events, EventMapper
+    , events
+    )
 
-{-| Autocomplete Styled
-
-This is still a work in progress.
+{-| Autocomplete View
 
 
 # Type
 
-@docs input
+@docs Events, EventMapper
+
+
+# Attributes
+
+@docs events
 
 -}
 
+import Html.Styled exposing (Attribute)
+import Html.Styled.Events as Events
+import Internal exposing (KeyDown(..), Msg(..))
+import Json.Decode as JD
 
-{-| Work in progress
+
+{-| Record to hold the events to be attached for input and every autocomplete choice
 -}
-input : String
-input =
-    "TODO Add elm-css Styled input component"
+type alias Events msg =
+    { inputEvents : List (Attribute msg)
+    , choiceEvents : Int -> List (Attribute msg)
+    }
+
+
+{-| Map Autocomplete Msg into your app's msg and also the msg to send when user selects a choice
+-}
+type alias EventMapper a msg =
+    { onSelect : msg
+    , mapHtml : Msg a -> msg
+    }
+
+
+{-| Returns the events to be attached for input and every autocomplete choice
+-}
+events : EventMapper a msg -> Events msg
+events mapper =
+    { inputEvents = inputEvents mapper
+    , choiceEvents = choiceEvents mapper
+    }
+
+
+inputEvents : EventMapper a msg -> List (Attribute msg)
+inputEvents mapper =
+    let
+        { mapHtml } =
+            mapper
+    in
+    [ Events.onInput (mapHtml << OnInput)
+    , Events.preventDefaultOn "keydown" <| onKeyDownDecoder mapper
+    ]
+
+
+choiceEvents : EventMapper a msg -> Int -> List (Attribute msg)
+choiceEvents mapper index =
+    let
+        { onSelect, mapHtml } =
+            mapper
+    in
+    -- We cannot use onClick twice
+    -- so we use onMouseDown/Up to send to Autocomplete first
+    -- and then onClick will send to user's app
+    -- onMouseDown/Up will always fire before onClick
+    -- See https://www.w3schools.com/jsref/event_onmouseup.asp
+    [ Events.onMouseDown <| mapHtml <| OnMouseDown index
+    , Events.onMouseUp <| mapHtml <| OnMouseUp index
+    , Events.onClick onSelect
+    ]
+
+
+onKeyDownDecoder : EventMapper a msg -> JD.Decoder ( msg, Bool )
+onKeyDownDecoder mapper =
+    let
+        { onSelect, mapHtml } =
+            mapper
+    in
+    JD.field "key" JD.string
+        |> JD.andThen
+            (\s ->
+                case s of
+                    "ArrowUp" ->
+                        JD.succeed ( mapHtml <| OnKeyDown ArrowUp, True )
+
+                    "ArrowDown" ->
+                        JD.succeed ( mapHtml <| OnKeyDown ArrowDown, True )
+
+                    "Enter" ->
+                        JD.succeed ( onSelect, True )
+
+                    _ ->
+                        JD.fail "Ignore other keys"
+            )
